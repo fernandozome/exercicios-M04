fd33bb59149f208426faa1f6d5050b0849c9bff0
Fernando Zome <fernandozome@gmail.com>
2023-10-25 17:14:54 -0300

    Concluindo implementação usando servidor mailtrap

--- Aula Envio de Emails/src/Controladores/login.js
+++ Aula Envio de Emails/src/Controladores/login.js
@@ -1,5 +1,9 @@
+const transport = require('../email')
+const compiladorHtlm = require('../utils/compliadorHtlm')
+
 const usuario = {
     email: "fernandozome@gmail.com",
+    nome: "Fernando Zome",
     senha: "123abc"
 
 }
@@ -16,6 +20,17 @@ const login = async (req, res) => {
         return res.status(400).json({ mensagem: "Email ou senha invalidos" })
     }
 
+
+    const html = await compiladorHtlm('./src/Templates/login.html', {
+        nomeusuario: usuario.nome
+    })
+
+    transport.sendMail({
+        from: `${process.env.EMAIL_NAME} <${process.env.EMAIL_FROM}>`,
+        to: `${usuario.nome} <${usuario.email}>`,
+        subject: "Tentativa de Login",
+        html
+    })
     return res.json({ mensagem: 'Login efetuado com sucesso' })
 }
 
